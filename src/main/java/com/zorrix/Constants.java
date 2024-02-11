ad11a69df252efd8613285c45dfe08637301d328
Andrew <ultschannel@gmail.com>
2024-02-11 14:35:42 +0200

    botVersion1.3

--- src/main/java/com/zorrix/Constants.java
+++ src/main/java/com/zorrix/Constants.java
@@ -3,5 +3,5 @@ package com.zorrix;
 public class Constants {
     public final static String botToken = "6427035917:AAHlKYY1uQ0jyFpFXJOtbmczo8pIemVUYO4";
     public final static String botName = "PLSchoolScheduleBot";
-    public final static String fileName = "src/main/java/com/zorrix/test.xlsx";
+    public final static String fileName = "src/main/resources/test.xlsx";
 }
