ef495f59cbf4a02b83e905e328201e0510b2d6ba
Andrew <ultschannel@gmail.com>
2024-02-11 18:40:58 +0200

    botVersion1.4

--- src/main/java/com/zorrix/Constants.java
+++ src/main/java/com/zorrix/Constants.java
@@ -4,7 +4,7 @@ package com.zorrix;
 public class Constants {
     public final static String BOT_TOKEN = "6427035917:AAHlKYY1uQ0jyFpFXJOtbmczo8pIemVUYO4";
     public final static String BOT_NAME = "PLSchoolScheduleBot";
-    public final static String FILE_NAME = "test.xlsx";
+    public final static String FILE_NAME = "schedule.xlsx";
     public final static String TODAY_SCHEDULE_MESSAGE = "Today's Schedule";
     public final static String TOMORROW_SCHEDULE_MESSAGE = "Tomorrow's Schedule";
     public final static String COMMAND_PREFIX = "/";
